0a6a1b94601a119b9f5dcf6273cf12eee087c451
Sharon Lee <sharonshlee@icloud.com>
2021-09-16 16:51:25 -0400

    add addProperty

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -167,10 +167,54 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
+// const propertyId = Object.keys(properties).length + 1;
+// property.id = propertyId;
+// properties[propertyId] = property;
+// return Promise.resolve(property);
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  // Property input object
+  // {
+  //   owner_id: int,
+  //   title: string,
+  //   description: string,
+  //   thumbnail_photo_url: string,
+  //   cover_photo_url: string,
+  //   cost_per_night: string,
+  //   street: string,
+  //   city: string,
+  //   province: string,
+  //   post_code: string,
+  //   country: string,
+  //   parking_spaces: int,
+  //   number_of_bathrooms: int,
+  //   number_of_bedrooms: int
+  // }
+  const queryString = `INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, 
+                                             street, city, province, post_code, country, 
+                                             parking_spaces, number_of_bathrooms, number_of_bedrooms) 
+                                     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) 
+                                     RETURNING *`;
+  const values = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms,
+  ];
+  return pool
+    .query(queryString, values)
+    .then((result) => result.rows) //return * properties
+    .catch((err) => {
+      err.message;
+    });
 };
 exports.addProperty = addProperty;
